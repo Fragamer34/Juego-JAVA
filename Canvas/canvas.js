0aa4efb41cae335bf11663bfa3f1ff84f2f76a98
Fragamer34 <franblaymar@gmail.com>
2025-01-30 20:38:30 +0100

    juego

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -1,6 +1,24 @@
 // Variables iniciales
 var x = 10, y = 0;
 let contador = 0;
+var semaforo1 = false;
+var contador1 = 0;
+var semaforo2 = false;
+var contador2 = 0;
+var semaforo3 = false;
+var contador3 = 0;
+var personaje1 = 0; // 0 nada, 1 aliado, 2 enemigo
+var personaje2 = 0; // 0 nada, 1 aliado, 2 enemigo
+var personaje3 = 0; // 0 nada, 1 aliado, 2 enemigo
+
+var alturaPersonaje = 200; // posicion y de la imagen del personaje
+
+var velocidad = 100; // milisegundos que dura el bucle de juego
+var pasoDeBucle = 25; // velocidad de proceso del bucle
+
+var coordX1 = 50; // Posicion X del personaje en puerta 1
+var coordX2 = 550;// Posicion X del personaje en puerta 2
+var coordX3 = 1050;// Posicion X del personaje en puerta 3
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
@@ -19,31 +37,30 @@ function dibujarElemento(id, posX, posY, width, height, visible= true) {
     else ctx.globalAlpha = 1;
 }
 
-function BorrarElemento(id, posX, posY, width, height) {
-    ctx.clearRect(posX, posY, width, height);
-    
-}
+// function BorrarElemento(id, posX, posY, width, height) {
+//     ctx.clearRect(posX, posY, width, height);
+// }
 
-// Dibuja los elementos detrás de la primera puerta
-dibujarElemento('Puerta Abierta', x, y, 500, 600);
-dibujarElemento('Enemigo', x + 150, y + 200, 200, 150); 
-dibujarElemento('Aliado', x + 200, y + 250, 150, 100); 
+// // Dibuja los elementos detrás de la primera puerta
+// dibujarElemento('Puerta Abierta', x, y, 500, 600);
+// dibujarElemento('Enemigo', x + 150, y + 200, 200, 150); 
+// dibujarElemento('Aliado', x + 200, y + 250, 150, 100); 
 
-// Dibuja la primera puerta al frente
-dibujarPrimeraPuerta();
+// // Dibuja la primera puerta al frente
+// dibujarPrimeraPuerta();
 
-// Segunda puerta
-x = 500;
-dibujarElemento('Puerta Abierta', x, y, 500, 600);
-dibujarElemento('Enemigo', x + 150, y + 200, 200, 150); 
-dibujarElemento('Aliado', x + 200, y + 250, 150, 100); 
-var imgPuerta2 = document.getElementById('Puerta');
-ctx.drawImage(imgPuerta2, x, y, 500, 600);
+// // Segunda puerta
+// x = 500;
+// dibujarElemento('Puerta Abierta', x, y, 500, 600);
+// dibujarElemento('Enemigo', x + 150, y + 200, 200, 150); 
+// dibujarElemento('Aliado', x + 200, y + 250, 150, 100); 
+// var imgPuerta2 = document.getElementById('Puerta');
+// ctx.drawImage(imgPuerta2, x, y, 500, 600);
 
-// Tercera puerta
-x = 1000;
-dibujarElemento('Puerta Abierta', x, y, 500, 600);
-dibujarElemento('Enemigo', x + 150, y + 200, 200, 150); 
-dibujarElemento('Aliado', x + 200, y + 250, 150, 100); 
-var imgPuerta3 = document.getElementById('Puerta');
-ctx.drawImage(imgPuerta3, x, y, 500, 600);
+// // Tercera puerta
+// x = 1000;
+// dibujarElemento('Puerta Abierta', x, y, 500, 600);
+// dibujarElemento('Enemigo', x + 150, y + 200, 200, 150); 
+// dibujarElemento('Aliado', x + 200, y + 250, 150, 100); 
+// var imgPuerta3 = document.getElementById('Puerta');
+// ctx.drawImage(imgPuerta3, x, y, 500, 600);
--- JS/Juego.js
+++ JS/Juego.js
@@ -1,19 +1,28 @@
 var STARTGAME = document.getElementById('STARTGAME');
 
+// AÑADIMOS UN LISTENER AL CLICK DEL BOTON de inicio
 STARTGAME.addEventListener('click', function(e) {
 
-console.log('Juego iniciado');
-STARTGAME.style.display = 'none';
-BucleTiempo();
+  console.log('Juego iniciado');
+  STARTGAME.style.display = 'none';
+  iniciar ();
+  BucleTiempo();
 
 })
+
+// añadimos un listener a la pulsacion de teclas
 document.addEventListener('keydown', function(event) {
 
     switch(event.key) {
         case "p":
             console.log('Tecla presionada: ', event.key);
             STARTGAME.style.display = 'Block';
-            contador = 5;
+
+            iniciar(); // limpiar todo (pintar puertas cerradas)
+
+            contador = 5; // FUERZA FI8N DEL BUCLE DE JUEGO
+
+//            alert("Juego terminado por usuario");
 
           break;
         case "z":
@@ -22,6 +31,18 @@ document.addEventListener('keydown', function(event) {
             Disparo.style.display = 'block';
             x = 10;
             dibujarElemento('Disparo', x + 200, y + 250, 150, 100);
+
+            if(personaje1 === 1)  { // hemos disparado a un aliado
+              // fin juego, hemos matado un aliado
+              STARTGAME.style.display = 'Block';
+//              iniciar(); // limpiar todo (pintar puertas cerradas)
+              contador = 5; // FUERZA FI8N DEL BUCLE DE JUEGO
+            }
+            if(personaje1 === 2)  { // hemos disparado a un enemigo
+              // matado enemigo, cerramos puerta y sumamos puntos
+              semaforo1 = false; // cerramos la puerta
+            }
+
             
           break;
         case "x":
@@ -42,30 +63,209 @@ document.addEventListener('keydown', function(event) {
       }
 })
 
+// Bucle de juego principal, se ejkecuta cada segundo y en el se controla toda la mecanica del juego
 function BucleTiempo() {
     
+  console.log("Iniciando el bucle del juego");
 
-console.log("Inicio del setInterval");
+  // Ejecuta esta función cada 2 segundos (2000 milisegundos)
+  let intervalo = setInterval(function() {
 
-// Ejecuta esta función cada 2 segundos (2000 milisegundos)
-let intervalo = setInterval(function() {
-  //contador++;
-  console.log("Contador:", contador);
+    // Detenemos el intervalo después de 5 repeticiones, 
+    // presionando P o cuando el jugador muere se iguala contador a 5 para romper el bucle
+    if (contador === 5) {
 
-  // Detenemos el intervalo después de 5 repeticiones
-  if (contador === 5) {
-    clearInterval(intervalo);
-    console.log("Intervalo detenido después de 5 repeticiones");
-  }
-  //Borrar disparos
+      clearInterval(intervalo);
+      console.log("Fin del bucle de juego");
+
+    }
+
+    //Abrir puertas aleatoriamente
+    AbrirPuertas();
+
+    //Antes de finalizar el bucle redibujar poantalla
+    RedibujarPantalla();
+    
+  }, velocidad); // fin timer
+
+} // Fin bucle juego
+
+//funcion inicia todas las variables
+function iniciar() {
+
+  // Inicializacion de variables
+  contador = 0;
+  semaforo1 = false;
+  contador1 = 0;
+  semaforo2 = false;
+  contador2 = 0;
+  semaforo3 = false;
+  contador3 = 0;
+  personaje1 = 0;
+  personaje2 = 0;
+  personaje3 = 0;
+  
+  // Pintamos puertas cerradas
   x= 5
   dibujarPrimeraPuerta();
-
-  x=500
+  x= 500
+  dibujarPrimeraPuerta();
+  x= 1000
   dibujarPrimeraPuerta();
 
-  x=1000
-  dibujarPrimeraPuerta();
-}, 2000);
 }
 
+// Funcion para controlar quer puertas se abren
+function AbrirPuertas(){
+
+  if(semaforo1 === false){ // Si la puerta esta cerrada hacemos el random para ver si se abre
+    AbrirPuerta(1,5);
+  } else {
+      contador1++;
+      console.log("Contador1:", contador1, Date.now());
+  }
+  if(contador1 === pasoDeBucle) { // si la puerta ya lleva abiuerta 5 segundos la cerramos
+      semaforo1 = false;
+  }
+
+  if(semaforo2 === false){ // Si la puerta esta cerrada hacemos el random para ver si se abre 
+      AbrirPuerta(2,500);
+  } else {
+     contador2++;
+     console.log("Contador2:", contador2);
+  }
+  if(contador2 === pasoDeBucle) {  // si la puerta ya lleva abiuerta 5 segundos la cerramos
+     semaforo2 = false;
+  }
+
+  if(semaforo3 === false){ // Si la puerta esta cerrada hacemos el random para ver si se abre
+    AbrirPuerta(3,1000);
+  } else {
+      contador3++;
+      console.log("Contador3:", contador3);
+  }
+  if(contador3 === pasoDeBucle) {  // si la puerta ya lleva abiuerta 5 segundos la cerramos
+      semaforo3 = false;
+  }
+
+  } // fin abrir puertas
+
+// Funcion quer genera un numero aleatorio y abre la puerta
+function AbrirPuerta(numPuerta, offset){
+
+  indicador = Math.random();
+  if(indicador < 0.1){
+
+    // se abre la puerta
+    x = offset;
+//    dibujarPuertaAbierta();;
+     switch(numPuerta) { // puerta para la que hemos hecho el random
+      case 1:
+        semaforo1 = true;
+        contador1 = 0;
+        break;
+      case 2:
+        semaforo2 = true;
+        contador2 = 0;
+        break;
+      case 3:
+        semaforo3 = true;
+        contador3 = 0;
+        break;
+     }
+
+     //  si se abre la puerta es pq hay que mostrar un aliado o enemigo
+     personaje = Math.random();
+     if (personaje < 0.5) { // aliado
+//      dibujarElemento('Aliado', offset, y, 500, 600);
+      switch(numPuerta) { // puerta para la que hemos hecho buscado personaje
+        case 1:
+          personaje1 = 1; // aliado es 1 en puerta 1 
+          break;
+        case 2:
+          personaje2 = 1; // aliado es 1 en puerta 2
+          break;
+        case 3:
+          personaje3 = 1; // aliado es 1 en puerta 3
+          break;
+       }
+       } 
+     else { // enemigo
+//      dibujarElemento('Enemigo', offset, y, 500, 600);
+      switch(numPuerta) { // puerta para la que hemos hecho buscado personaje
+        case 1:
+          personaje1 = 2; // enemigo es 2 en puerta 1 
+          break;
+        case 2:
+          personaje2 = 2; // enemigo es 2 en puerta 2
+          break;
+        case 3:
+          personaje3 = 2; // enemigo es 2 en puerta 3
+          break;
+       }
+     } 
+    
+  }
+
+}
+
+function dibujarPuertaAbierta(offset){
+  dibujarElemento('Puerta Abierta', offset, y, 500, 600);
+}
+
+function RedibujarPantalla() {
+  //Borrar disparos
+  if (semaforo1 === true) {
+    dibujarPuertaAbierta(5);
+    if(personaje1 === 1){ // es un aliado
+      dibujarAliado(coordX1);
+    }  
+    else if(personaje1 === 2) { // es un enemigo
+      dibujarEnemigo(coordX1);
+    }
+
+  }
+  else {
+    x= 5
+  dibujarPrimeraPuerta();
+  }
+  
+
+  if (semaforo2 === true) {
+    dibujarPuertaAbierta(500);
+    if(personaje2 === 1){ // es un aliado
+      dibujarAliado(coordX2);
+    }  
+    else if(personaje2 === 2) { // es un enemigo
+      dibujarEnemigo(coordX2);
+    }
+
+  }
+  else{
+    x= 500
+  dibujarPrimeraPuerta();
+  }
+
+  if (semaforo3 === true) {
+    dibujarPuertaAbierta(1000);
+    if(personaje3 === 1){ // es un aliado
+      dibujarAliado(coordX3);
+    }  
+    else if(personaje3 === 2) { // es un enemigo
+      dibujarEnemigo(coordX3);
+    }
+
+  }
+  else{
+    x= 1000;
+  dibujarPrimeraPuerta();
+  }
+}
+
+function dibujarAliado(offset){
+  dibujarElemento('Aliado', offset, alturaPersonaje, 350, 300);
+}
+
+function dibujarEnemigo(offset){
+  dibujarElemento('Enemigo', offset, alturaPersonaje, 350, 300);
+}
